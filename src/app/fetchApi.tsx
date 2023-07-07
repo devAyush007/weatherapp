13d4bc2e391870000ad3ec36e9b5af0b932b1a6a
Ayush <you@example.com>
2023-07-07 20:00:16 +0530

    Error Fixed?

--- src/app/fetchApi.tsx
+++ src/app/fetchApi.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import "./globals.css";
+import Image from "next/image";
 
 const FetchApi = () => {
   const [query, setQuery] = useState("");
@@ -19,58 +20,57 @@ const FetchApi = () => {
 
   return (
     <>
-      <div>
-        <form
-          className="flex flex-col my-8 py-8 items-center justify-center text-white"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="text-white font-bold  my-4">Weather Details</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <form className="my-8 py-8 text-black" onSubmit={handleSubmit}>
+          <h1 className="text-3xl font-bold mb-4 text-center">
+            Weather Details
+          </h1>
 
           <label>
-            CITY :
+            CITY:
             <input
               type="text"
               placeholder="City"
-              className="text-black rounded-xl p-2 mx-3"
+              className="rounded-xl p-2 mx-3"
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
           </label>
           <br />
-          <button className="text-white" type="submit">
+          <br />
+          <button className="mx-2 px-2" type="submit">
             <span className="box">Submit</span>
           </button>
-          <hr />
-          <br />
         </form>
         {weatherData && (
-          <div className="bg-white flex flex-col items-center justify-center rounded-3xl card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
+          <div className="flex items-center justify-center">
+            <div className="bg-white rounded-3xl w-96 shadow-xl p-8">
               <div>
-                <p className="card-title">
+                <p className="text-lg font-bold mb-4">
                   Description: {weatherData.weather[0].description}
                 </p>
                 <p>Temperature: {weatherData.main.temp.toFixed(2)}°C</p>
                 <p>Humidity: {weatherData.main.humidity}%</p>
                 <p>Condition: {weatherData.weather[0].main}</p>
-                <div className="flex card card-compact w-96 bg-base-100 shadow-xl">
-                  <div className="card-body">
-                    <h2 className="card-title">Wind Speed</h2>
-                    <p> {weatherData.wind.speed} km/h</p>
-                    <figure>
-                      <img
-                        src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
-                        alt="icon"
-                      />
-                    </figure>
-                  </div>
+                <div className="bg-gray-200 rounded-xl p-4 mt-4">
+                  <h2 className="text-xl font-bold mb-2">Wind Speed</h2>
+                  <p>{weatherData.wind.speed} km/h</p>
+                  <figure>
+                    <Image
+                      src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+                      alt="Weather Icon"
+                      className="mt-4"
+                      height={80}
+                      width={80}
+                    />
+                  </figure>
                 </div>
               </div>
             </div>
           </div>
         )}
       </div>
-      <h3 className="text-red-600 absolute bottom-1">
+      <h3 className="text-red-600 text-center mb-4">
         NOTE: THIS IS NOT 100% ACCURATE DATA
       </h3>
     </>
